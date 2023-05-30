af5a2b42112ded94561961d93a4fd1aba7ca9b82
mohcine <mohcine.badour@uit.ac.ma>
2023-05-30 16:37:46 +0100

    100%

--- resources/views/candidats/candidat-dashboard.blade.php
+++ resources/views/candidats/candidat-dashboard.blade.php
@@ -209,8 +209,19 @@
     </div>
 
     {{-- ! --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+    <style>
+        .search-familly {
+            width: 100%;
+            height: 60px;
+            border: 1px solid rgba(235, 236, 241, 1);
+            padding: 0 30px;
+            font-size: 15px;
+            color: #000000;
+            border-radius: 5px;
+        }
+    </style>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #ffffff;">
+        <div class="collapse navbar-collapse offset-2" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#"><i class="fas fa-gauge"></i> Dashboard <span
@@ -226,9 +237,11 @@
                     <a class="nav-link" href="#"><i class="fa-solid fa-user"></i> My Account</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+            <form class="form-inline my-2 my-lg-0 ">
+                <input class="form-control mr-sm-2 search-familly" type="search" placeholder="Search"
+                    aria-label="Search">
+                {{-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> --}}
             </form>
         </div>
     </nav>
