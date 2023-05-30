9c875e1b59af5affbad025b2eb4c4f8403139347
mohcine <mohcine.badour@uit.ac.ma>
2023-05-30 16:08:25 +0100

    affiche anonce

--- resources/views/candidats/candidat-dashboard.blade.php
+++ resources/views/candidats/candidat-dashboard.blade.php
@@ -64,10 +64,9 @@
     </script>
 
 
-    @livewireStyles
 </head>
 
-<body style="background-color: #FFF">
+<body background="{{ asset('img/back-blur.png') }}" style="background-color: #FFF">
     <div class="container-fluid bg-white p-0">
         <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
             <a href="{{ route('welcome') }}"
@@ -208,7 +207,54 @@
             </div>
         </nav>
     </div>
-    @livewireScripts
+
+    {{-- ! --}}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-gauge"></i> Dashboard <span
+                            class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-message"></i> Conversation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-puzzle-piece"></i> My Ad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-user"></i> My Account</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    {{-- <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="collapse navbar-collapse ms-5" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-gauge"></i> Dashboard <span
+                            class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-message"></i> Conversation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-puzzle-piece"></i> My Ad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-user"></i> My Account</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav> --}}
 </body>
 
 </html>
