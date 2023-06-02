b43c2f18cf5cc1404c89aedbcb0885ddf68df28a
mohcine <mohcine.badour@uit.ac.ma>
2023-06-02 10:49:55 +0100

    ok

--- resources/views/candidats/profile.blade.php
+++ resources/views/candidats/profile.blade.php
@@ -10,7 +10,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css"
+        integrity="sha512-SgaqKKxJDQ/tAUAAXzvxZz33rmn7leYDYfBP+YoMRSENhf3zJyx3SBASt/OfeQwBHA1nxMis7mM3EV/oYT6Fdw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         /* body {
             background: #f7f7ff;
@@ -38,7 +43,7 @@
 </head>
 
 <body>
-    <div class="container-fluid bg-white p-0">
+    <div class="container-fluid bg-secondary p-0">
         <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
             <a href="{{ route('welcome') }}"
                 class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
@@ -49,7 +54,7 @@
                 data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
                 <div class="navbar-nav ms-auto p-4 p-lg-0">
                     <a href="#" class="nav-item nav-link">Help</a>
                     {{-- <a href="index.html" class="nav-item nav-link active">Home</a> --}}
@@ -85,7 +90,7 @@
                         list-style-type: none;
                     }
                 </style>
-                <li class="nav-item dropdown has-arrow main-drop">
+                <li class="nav-item dropdown has-arrow main-drop ">
                     <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                         <span class="user-img">
                             <img src="{{ asset('img/profile.png') }}" alt="" style="width: 30px">
@@ -172,13 +177,34 @@
                         margin: 15px;
                         box-shadow: 0 2px 10px rgba(54, 54, 54, .2);
                     }
+
+                    .profile .btn-upgrade {
+                        height: 50px;
+                        background: transparent;
+                        cursor: pointer;
+                        background-color: #4fb8a3;
+                        color: #fff;
+                        border: 2px solid #4fb8a3;
+                        padding: 5px;
+                        border-radius: 6px;
+                        box-sizing: border-box;
+                        font-size: 11px;
+                        font-weight: 600;
+                        text-align: center;
+                        text-decoration: none;
+                        transition: background-color .3s, border-color .3s;
+                        margin: 0px;
+                        box-shadow: 0 2px 10px rgba(54, 54, 54, .2);
+                    }
                 </style>
                 <button type="button" class="btn-upgrade">
                     Get Verified
                 </button>
             </div>
         </nav>
+
     </div>
+
     <div class="container mt-5">
         <div class="main-body">
             <div class="row">
@@ -188,10 +214,11 @@
                             <div class="d-flex flex-column align-items-center text-center">
                                 <img src="{{ asset('img/maid-ic.png') }}" alt="Admin" class="rounded-circle p-1"
                                     width="110">
-                                <div class="mt-3">
+                                <div class=" profile mt-3">
                                     <h4>USERNAME</h4>
                                     <p class="text-secondary mb-1">ADRESSE</p>
                                     <p class="text-muted font-size-sm">COUNTRY</p>
+                                    {{-- <button type="button" class="btn-upgrade w-100">Get Verified</button> --}}
                                     {{-- <button class="btn btn-primary">Follow</button> --}}
                                     {{-- <button class="btn btn-outline-primary">Message</button> --}}
                                 </div>
@@ -199,17 +226,17 @@
                             <hr class="my-4">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-globe me-2 icon-inline">
-                                            <circle cx="12" cy="12" r="10"></circle>
-                                            <line x1="2" y1="12" x2="22" y2="12">
-                                            </line>
+                                    <h6 class="mb-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round"
+                                            class="feather feather-mail me-2 icon-inline">
                                             <path
-                                                d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z">
-                                            </path>
-                                        </svg>Gmail</h6>
+                                                d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+                                            <polyline points="22,6 12,13 2,6" />
+                                        </svg>
+                                        Gmail
+                                    </h6>
                                     <span class="text-secondary">HERE EMAIL</span>
                                 </li>
                                 <li
@@ -217,50 +244,13 @@
                                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24"
                                             height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-github me-2 icon-inline">
-                                            <path
-                                                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
-                                            </path>
-                                        </svg>Github</h6>
-                                    <span class="text-secondary">bootdey</span>
-                                </li>
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-twitter me-2 icon-inline text-info">
-                                            <path
-                                                d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
-                                            </path>
-                                        </svg>Twitter</h6>
-                                    <span class="text-secondary">@bootdey</span>
-                                </li>
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-instagram me-2 icon-inline text-danger">
-                                            <rect x="2" y="2" width="20" height="20"
-                                                rx="5" ry="5"></rect>
-                                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                                            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5">
-                                            </line>
-                                        </svg>Instagram</h6>
-                                    <span class="text-secondary">bootdey</span>
-                                </li>
-                                <li
-                                    class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                            class="feather feather-facebook me-2 icon-inline text-primary">
+                                            class="feather feather-facebook me-2 icon-inline ">
                                             <path
                                                 d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z">
                                             </path>
                                         </svg>Facebook</h6>
-                                    <span class="text-secondary">bootdey</span>
+
+                                    <span class="text-secondary">HERE FACEBOOK</span>
                                 </li>
                             </ul>
                         </div>
@@ -290,7 +280,7 @@
                                     <h6 class="mb-0">Mobile</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="(320) 380-4539">
+                                    <input type="text" class="form-control" value="" autocomplete="off">
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -298,7 +288,7 @@
                                     <h6 class="mb-0">Address</h6>
                                 </div>
                                 <div class="col-sm-9 text-secondary">
-                                    <input type="text" class="form-control" value="Bay Area, San Francisco, CA">
+                                    <input type="text" class="form-control" value="" autocomplete="off">
                                 </div>
                             </div>
                             <div class="row">
