425d2453fc48426afb2de756445d925464465c78
[your <mohamed.boukbir@uit.ac.ma>
2023-06-07 16:35:54 +0100

    ok

--- app/Http/Controllers/DashbordController.php
+++ app/Http/Controllers/DashbordController.php
@@ -58,29 +58,32 @@ class DashbordController extends Controller
 
     public function usernoroleAtacher($user)
     {
-        session()->put('role', $user);
-        return redirect()->route('users.validerrole');
-
+        // dd($user);
+        $users = Auth::user();
+        $users->$user;
+        $users->save();
+         $users->attachRole($user);
+        return redirect()->route('dashboard');
     }
     /******************** assurer le role********************************************* */
     // detacherRol 
 
-    public function valideRole()
-    {
-      return view('front.validerRole'); 
-    }
+    // public function valideRole()
+    // {
+    //   return view('front.validerRole'); 
+    // }
 
-    public function valideRoleAtach()
-    {
-       $users = Auth::user();
-       $users->type=session()->get('role');
-    //    dd('nadi');
-       $users->save();
-        $users->attachRole(session()->get('role'));
-        session()->forget('role');
+    // public function valideRoleAtach()
+    // {
+    //    $users = Auth::user();
+    //    $users->type=session()->get('role');
+    // //    dd('nadi');
+    //    $users->save();
+    //     $users->attachRole(session()->get('role'));
+    //     session()->forget('role');
 
-        return  redirect()->route('dashboard'); 
-    }
+    //     return  redirect()->route('dashboard'); 
+    // }
 
      /******************** assurer le role********************************************* */
     //
