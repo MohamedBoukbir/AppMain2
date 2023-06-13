e0b7fb1512c15da540cdda96d20dfe9fec92c434
mohcine <mohcine.badour@uit.ac.ma>
2023-06-13 23:38:45 +0100

    ok

---
+++ resources/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFkn7Sh0ab54wpAEhvrdW4uwy-IFpLSFE",
+  authDomain: "badourproject.firebaseapp.com",
+  projectId: "badourproject",
+  storageBucket: "badourproject.appspot.com",
+  messagingSenderId: "159735364873",
+  appId: "1:159735364873:web:d6a67667f164db285ea7d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
